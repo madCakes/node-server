be1b65d0807a175a84c7e3f904691dbf45a6542d
madCakes <jason.pbrooks@hotmail.com>
2022-07-14 14:07:41 +0100

    Matt/routing solution

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  // server logicw
+  // server logic
+
+  console.log(req.url);
+
+  // Create different "routes" depending on the value of req.url
+  // Send different responses depending on what is being requested
+  // 1. Home route "/" send back "Welcome to the crazy cat corner!"
+  // 2. Cats routes "/cats" send back some info about cats. Start with string, then try
+  // returning some JSON
+  // 3. Create a default route that catches everything else and returns not found.
+  let body, code;
+  switch (req.url) {
+    case "/":
+      body = "Welcome to the crazy cat corner";
+      code = 200;
+      break;
+    case "/cats":
+      body = "My cat's name is cleo";
+      code = 200;
+      break;
+    default:
+      body = `${req.url} does not exist`;
+      code = 404;
+  }
+
+  res.statusCode = code;
+  res.end(body);
+
+  res.statusCode = 404;
+  res.end("Hello, World!");
 });
 
 // listen(port)
-server.listen(80, () => {
-  console.log("server running on port 80");
+server.listen(3000, () => {
+  console.log("server running on port 3000");
 });
